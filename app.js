7ed93f17b0563853cf6ed3f8ef52721fed3022fd
Matthew Zimmerman <s20400608@gmail.com>
2023-10-29 15:12:53 -0700

    changed files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let isModalOpen = false
 let contrastToggle = false;
-const scaleFactor = 1/20;
+const scaleFactor = 1/16;
 
 function moveBackground(event){
     const shapes = document.querySelectorAll(".shape")
